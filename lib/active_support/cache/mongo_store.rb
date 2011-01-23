239c6afba8e78672aad00895d1dc97efc8eabb1f
Peter Haza <peter.haza@gmail.com>
2011-01-24 00:12:47 +0100

    Backwards compatibility

--- lib/active_support/cache/mongo_store.rb
+++ lib/active_support/cache/mongo_store.rb
@@ -81,7 +81,7 @@ module MongoStore
       end
       def read_entry(key, options=nil)
         doc = collection.find_one('_id' => key, 'expires' => {'$gt' => Time.now})
-        Marshal.load(doc['value']) rescue doc['value'] if doc
+        Marshal.load(doc['value']) rescue ActiveSupport::Cache::Entry.new(doc['value']) if doc
       end
       def delete_entry(key, options=nil)
         collection.remove({'_id' => key})
